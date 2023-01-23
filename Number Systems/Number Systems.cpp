f8cbdc9840997bb7122f810595cdded474d0280f
NiTzuA <11v35faustin@gmail.com>
2023-01-23 09:47:19 +0800

    v1.0b
    - Added Binary to Octal
    - Added Binary to Decimal

--- Number Systems/Number Systems.cpp
+++ Number Systems/Number Systems.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <conio.h>
+#include <cmath>
 using namespace std;
 
 /* TO DO LIST: 
 
 1. Converter (Decimal)
     1.1 Decimal to Binary
-    1.2 Decimal to Octal
+    1.2 Decimal to Octal 
     1.3 Decimal to Hexadecimal
 
 2. Converter (Binary)
-    2.1 Binary to Octal
-    2.2 Binary to Decimal
+    2.1 Binary to Octal             DONE
+    2.2 Binary to Decimal           DONE
     2.3 Binary to Hexadecimal
 
 3. Converter (Octal)
@@ -47,23 +49,119 @@ using namespace std;
 
 */
 
+void conBin() {
 
+    int binary = 0;
+    int octal = 0;
+    int decimal = 0;
+    int hexadecimal = 0;
+    int modBin = 0;
+    int modDec = 0;
+
+    cout << "Convert from Binary to ______:" << endl
+        << "\n[1] Octal" << endl
+        << "[2] Decimal" << endl
+        << "[3] Hexadecimal" << endl
+        << "[4] End Program" << endl;
+
+    char choice;
+    choice = _getch();
+
+    if (choice == '1') {
+        system("cls");
+        cout << "Enter a Binary Number: ";
+        cin >> binary;
+
+        // Conversion begins here.
+        
+        modBin = binary;
+        int i = 0;
+
+        do {
+            modBin = binary % 10;
+            decimal += modBin * pow(2, i);
+            binary /= 10;
+            i++;
+        } while (binary != 0);
+
+        modDec = decimal;
+        i = 0;
+
+        do {
+            modDec = decimal % 8;
+            octal += modDec * pow(10, i);
+            decimal /= 8;
+            i++;
+        } while (decimal != 0);
+        
+        cout << "\nConverted number is: " << octal;
+    } 
+    else if (choice == '2') {
+        system("cls");
+
+        cout << "Enter a Binary Number: ";
+        cin >> binary;
+
+        // Conversion begins here.
+
+        modBin = binary;
+        int i = 0;
+
+        do {
+            modBin = binary % 10;
+            decimal += modBin * pow(2, i);
+            binary /= 10;
+            i++;
+        } while (binary != 0);
+
+        cout << "\n\nConverted number is: " << decimal;
+
+    }
+    else if (choice == '3') {
+        system("cls");
+
+
+    }
+    else if (choice == '4') {
+        system("cls");
+
+
+    }
+    else {
+
+    }
+
+}
 
 int main()
 {
+    char loop = 'y';
+    conBin();
+
+    /*while (loop == 'y') {
+
+        system("cls");
+        
+        cout << "Welcome to Austin's Number System Converter!" << endl
+            << "Please select the number on your keyboard to select your desired action." << endl
+            << "\n[1] Convert From Binary" << endl
+            << "[2] Convert From Octal" << endl
+            << "[3] Convert From Decimal" << endl
+            << "[4] Convert From Hexadecimal" << endl
+            << "[5] Binary Practice Conversions" << endl
+            << "[6] Octal Practice Conversions" << endl
+            << "[7] Decimal Practice Conversions" << endl
+            << "[8] Hexadecimal Practice Conversions" << endl;
+
+        char choice;
+        choice = _getch();
+
+        
+        system("cls");
+
+        cout << "Would you like to use the program again? (y/n):" << endl;
+        loop = _getch();
+        
+    }*/
     
-    cout << "Welcome to Austin's Number System Converter!" << endl
-        << "Please select the number on your keyboard to select your desired action." << endl
-        << "\n[1] Convert From Binary" << endl
-        << "[2] Convert From Octal" << endl
-        << "[3] Convert From Decimal" << endl
-        << "[4] Convert From Hexadecimal" << endl
-        << "[5] Binary Practice Conversions" << endl
-        << "[6] Octal Practice Conversions" << endl
-        << "[7] Decimal Practice Conversions" << endl
-        << "[8] Hexadecimal Practice Conversions" << endl;
-
-    
-
-
 }
