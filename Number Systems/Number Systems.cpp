44764c14f281a5d93003d849998dea5385de498e
NiTzuA <11v35faustin@gmail.com>
2023-01-23 08:24:02 +0800

    Add project files.

---
+++ Number Systems/Number Systems.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+/* TO DO LIST: 
+
+1. Converter (Decimal)
+    1.1 Decimal to Binary
+    1.2 Decimal to Octal
+    1.3 Decimal to Hexadecimal
+
+2. Converter (Binary)
+    2.1 Binary to Octal
+    2.2 Binary to Decimal
+    2.3 Binary to Hexadecimal
+
+3. Converter (Octal)
+    3.1 Octal to Binary
+    3.2 Octal to Decimal
+    3.3 Octal to Hexadecimal
+
+4. Converter (Hexadecimal)
+    4.1 Hexadecimal to Binary
+    4.2 Hexadecimal to Octal
+    4.3 Hexadecimal to Decimal
+
+5. Practice Problems (Decimal)
+    5.1 Decimal to Binary
+    5.2 Decimal to Octal
+    5.3 Decimal to Hexadecimal
+
+6. Practice Problems (Binary)
+    6.1 Binary to Octal
+    6.2 Binary to Decimal
+    6.3 Binary to Hexadecimal
+
+7. Practice Problems (Octal)
+    7.1 Octal to Binary
+    7.2 Octal to Decimal
+    7.3 Octal to Hexadecimal
+
+8. Practice Problems (Hexadecimal)
+    8.1 Hexadecimal to Binary
+    8.2 Hexadecimal to Octal
+    8.3 Hexadecimal to Decimal
+
+9. Practice Problems (Random)
+
+*/
+
+
+
+int main()
+{
+    
+    cout << "Welcome to Austin's Number System Converter!" << endl
+        << "Please select the number on your keyboard to select your desired action." << endl
+        << "\n[1] Convert From Binary" << endl
+        << "[2] Convert From Octal" << endl
+        << "[3] Convert From Decimal" << endl
+        << "[4] Convert From Hexadecimal" << endl
+        << "[5] Binary Practice Conversions" << endl
+        << "[6] Octal Practice Conversions" << endl
+        << "[7] Decimal Practice Conversions" << endl
+        << "[8] Hexadecimal Practice Conversions" << endl;
+
+    
+
+
+}
